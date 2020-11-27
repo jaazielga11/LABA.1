2507077683dce4d90cc872abc197ca54a00854ac
Jaaziel Gaitan <jaaziel.gaitan@utp.ac.pa>
2020-11-26 19:59:38 -0500

    Agregar archivos de proyecto.

---
+++ LABA.1/LABA.1.py
@@ -0,0 +1,17 @@
+print('Hello world')
+### Introducir un numero por teclado y decir si es par o impar
+num = int(input('Introduzca un numero: '))
+ifnum % 2 == 0:
+print('Par')
+else:
+print('Impar')
+
+###Ejemplo for, imprime los 20 primeros numeros en una linea
+for i in range(20):
+print(i, end=" ") #imprimir numero, sin salto linea
+print() #lineavacia
+
+##Ejemplo foreach, imprime las letras TEXTO
+for i in "TEXTO":
+print(i)
+
